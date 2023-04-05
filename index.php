d587d0ff5a1758ef75b50351305ebaa32890d105
TrojanB <qwertroya@gmail.com>
2023-04-05 09:36:01 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forum o psach</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+
+    <div id="baner">
+        <h1>Forum miłośników psów</h1>
+    </div>
+
+    <div id="lewy">
+        <img src="zad1/Avatar.jpg" alt="Użytkownik forum">
+        <?php
+
+        ?>
+
+        <video controls="controls" src="zad1/video.mp4"></video>
+    </div>
+
+    <div id="prawy">
+        <form action="" method="POST">
+
+            <input type="submit" value="Dodaj opdpowiedź">
+        </form>
+        <h2>Odpowiedzi na pytanie</h2>
+        <ol>
+            <?php
+
+            ?>
+        </ol>
+    </div>
+
+    <footer>Autor: Trojan Bogacki 3pir <a href="http://mojestrony.pl/">Zobacz nasze realizacje</a></footer>
+
+</body>
+</html>
