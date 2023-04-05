e0bc0f60db6495af41bb177566a781c139d48b54
TrojanB <qwertroya@gmail.com>
2023-04-05 12:30:15 +0200

    fdsfs chyba jakos skonczone nienajlejfads

--- index.php
+++ index.php
@@ -15,28 +15,57 @@
     </div>
 
     <div id="lewy">
-        <img src="zad1/Avatar.jpg" alt="Użytkownik forum">
+        <img src="zad1/Avatar.png" alt="Użytkownik forum">
         <?php
+            $db = mysqli_connect('localhost', 'root', '', 'forumpsy');
+            $kw = "SELECT konta.nick, konta.postow, pytania.pytanie FROM konta INNER JOIN pytania ON konta.id=pytania.konta_id WHERE pytania.id = 1;";
+            $w = mysqli_query($db, $kw);
+            while($p = mysqli_fetch_row($w)){
+                echo "<h4>Użytkownik: $p[0]</h4> <br>";
+                echo "<p>$p[1] postów na forum</p> <br>";
+                echo "<p>$p[2]</p> <br>";
+            }
+            mysqli_close($db);
 
         ?>
 
-        <video controls="controls" src="zad1/video.mp4"></video>
+        <video controls loop>
+            <source src="zad1/video.mp4">
+        </video>
     </div>
 
     <div id="prawy">
         <form action="" method="POST">
+            <textarea name="txta" rows="4" cols="40"></textarea>
 
             <input type="submit" value="Dodaj opdpowiedź">
         </form>
+        <?php
+        if(isset($_POST['txta'])){
+            $db = mysqli_connect('localhost', 'root', '', 'forumpsy');
+            $tx = $_POST['txta'];
+            $kw = "INSERT INTO odpowiedzi VALUES(8,1,5, '$tx');";
+            mysqli_query($db, $kw);
+            mysqli_close($db);
+        }
+        ?>
         <h2>Odpowiedzi na pytanie</h2>
         <ol>
             <?php
+                $db = mysqli_connect('localhost', 'root', '', 'forumpsy');
+                $kw = "SELECT odpowiedzi.id, odpowiedzi.odpowiedz, konta.nick FROM odpowiedzi INNER JOIN konta ON odpowiedzi.konta_id = konta.id;";
+
+                $w = mysqli_query($db, $kw);
+                while($p = mysqli_fetch_row($w)){
+                    echo "<li>$p[1] <i>$p[2]</i> <hr></li>";
+                }
+                mysqli_close($db);
 
             ?>
         </ol>
     </div>
 
-    <footer>Autor: Trojan Bogacki 3pir <a href="http://mojestrony.pl/">Zobacz nasze realizacje</a></footer>
+    <footer>Autor: Trojan Bogacki 3pir <a href="http://mojestrony.pl/" target="_blank">Zobacz nasze realizacje</a></footer>
 
 </body>
 </html>
